1a8dea639633d2143f5efd1a431eaa277ec29244
Fahad Sadah <fahadsadah@gmail.com>
2010-09-15 17:29:29 +0100

    Correcting obvious error

--- reload.rb
+++ reload.rb
@@ -1,7 +1,7 @@
 on(:privmsg) do
 	|e|
 	if $config['admin'] == "#{e['nick']}!#{e['user']}@#{['host']}"
-		if e['message'].split[0] == reload
+		if e['message'].split[0] == '\reload'
 			Dir[File.dirname(__FILE__) + '/scripts/*'].each { |file| require file }
 		end
 	end

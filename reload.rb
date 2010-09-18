a7e837e677ba9005156be24d80c943e2f4beb1fd
Fahad Sadah <fahadsadah@gmail.com>
2010-09-18 17:36:53 +0100

    Removed reload command

--- reload.rb
+++
@@ -1,6 +0,0 @@
-on(:command, {:command => 'reload'}) do
-	|e|
-	if $config['admin'] == "#{e['nick']}!#{e['user']}@#{['host']}"
-		Dir[File.dirname(__FILE__) + '/scripts/*'].each { |file| require file }
-	end
-end
